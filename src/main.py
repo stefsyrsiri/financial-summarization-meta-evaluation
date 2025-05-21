1226cd121a5d24a7127519fed7f82277634dc546
Stefania Syrsiri <stefsyrsiri@gmail.com>
2025-05-21 22:24:17 +0300

    fix: Fixed the logger, added suppresion for tensorflow.

--- src/main.py
+++ src/main.py
@@ -1,54 +1,66 @@
 """
 Main script for running construction of noisy candidate summaries and scoring them.
 
-This script initializes the DataCollector, SummaryCorruptor, SummaryGenerator,
+Initializes the DataCollector, SummaryCorruptor, SummaryGenerator,
 and SummaryEvaluator classes to collect the Greek data, generates new noisy
 summaries based on the Greek data and evaluates them against their original
 version.
 """
 
 import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # TensorFlow suppress info/warning
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 from argparse import ArgumentParser
 
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)  # TF 1.x legacy logs
 from dotenv import load_dotenv
 from loguru import logger
+from transformers.utils import logging as hf_logging
+hf_logging.set_verbosity_error()  # Huggingface warnings
 
 from pipelines.collect import collect_data
 from pipelines.generate import generate_noisy_summaries
 from pipelines.evaluate import evaluate_summaries
 
 
-# Load from .env
 load_dotenv(override=True)
-LANGUAGE = os.getenv('LANGUAGE')
-ANNUAL_REPORTS_DIR = os.getenv('ANNUAL_REPORTS_DIR')
-GOLD_SUMMARIES_DIR = os.getenv('GOLD_SUMMARIES_DIR')
-CANDIDATE_SUMMARIES_DIR = os.getenv('CANDIDATE_SUMMARIES_DIR')
-RESULTS_PATH = os.getenv('RESULTS_PATH')
-SUMMARY_VER = os.getenv('SUMMARY_VER')
-FILE_EXTENSION = os.getenv('FILE_EXTENSION')
+LANGUAGE = os.getenv("LANGUAGE")
+ANNUAL_REPORTS_DIR = os.getenv("ANNUAL_REPORTS_DIR")
+GOLD_SUMMARIES_DIR = os.getenv("GOLD_SUMMARIES_DIR")
+CANDIDATE_SUMMARIES_DIR = os.getenv("CANDIDATE_SUMMARIES_DIR")
+RESULTS_PATH = os.getenv("RESULTS_PATH")
+SUMMARY_VER = os.getenv("SUMMARY_VER")
+FILE_EXTENSION = os.getenv("FILE_EXTENSION")
 
-# TODO: Fix logger
-# Configure logger
-logger.add('logs/main_{time}.log', rotation='1 day', compression='zip', level='DEBUG', filter=lambda record: record["level"].name == "DEBUG")
-logger.add('logs/errors_{time}.log', rotation='1 day', compression='zip', level='ERROR', filter=lambda record: record["level"].name == "ERROR")
+# Silence the logger
+logger.remove()
+
+# Terminal
+logger.add(sys.stdout, level="INFO", enqueue=True)
+
+# File
+logger.add("logs/main_{time}.log", rotation="1 day", compression="zip", level="DEBUG")
 
 
 @logger.catch
 def main():
-    logger.info(f'Starting the main process for {LANGUAGE}.')
+    logger.info(f"Starting the main process for {LANGUAGE}.")
+
     parser = ArgumentParser(description="Run pipeline steps.")
 
     # Parse args
-    parser.add_argument('--collect', action='store_true', help="Collect data")
-    parser.add_argument('--generate', action='store_true', help="Generate noisy summaries")
-    parser.add_argument('--evaluate', action='store_true', help="Evaluate summaries")
-    parser.add_argument('--all', action='store_true', help="Run all steps")
+    parser.add_argument("--collect", action="store_true", help="Collect data")
+    parser.add_argument("--generate", action="store_true", help="Generate noisy summaries")
+    parser.add_argument("--evaluate", action="store_true", help="Evaluate summaries")
+    parser.add_argument("--all", action="store_true", help="Run all steps")
 
     # Subset of the source documents
-    source_docs = [file[:-4] for file in os.listdir(ANNUAL_REPORTS_DIR)][:10]  # :-4 removes the file extension
+    source_docs = [file[:-4] for file in os.listdir(ANNUAL_REPORTS_DIR)]  # :-4 removes the file extension
     logger.info(f"Loaded {len(source_docs)} annual reports for noisy summary generation.")
     logger.info(f"Loaded {source_docs}.")
 
@@ -73,5 +85,5 @@ def main():
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
