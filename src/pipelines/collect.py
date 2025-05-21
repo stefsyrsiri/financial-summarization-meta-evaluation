bdc7cd3b770df7473e9ed35afca151aaacc9afec
Stefania Syrsiri <stefsyrsiri@gmail.com>
2025-05-21 22:33:20 +0300

    style: Changed formatting to double quotes.

--- src/pipelines/collect.py
+++ src/pipelines/collect.py
@@ -6,10 +6,10 @@ from modules.data_collector import DataCollector
 def collect_data():
     # Collect data for destruction and evaluation
     data_collector = DataCollector()
-    logger.info('Collecting data.')
+    logger.info("Collecting data.")
     try:
         data_collector.collect_data()
-        logger.info('Successfully collected Greek data.')
+        logger.info("Successfully collected Greek data.")
     except Exception as e:
-        logger.error(f'Failed data collection: {e}')
+        logger.error(f"Failed data collection: {e}")
         return
